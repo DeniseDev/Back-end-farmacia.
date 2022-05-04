ef131437ea3ce148656f37d12bf4ccbdc7802ef2
DeniseDev <101351123+DeniseDev@users.noreply.github.com>
2022-05-04 16:57:51 -0300

    Add files via upload

---
+++ FarmaVidaApplication.java
@@ -0,0 +1,13 @@
+package com.farmavida.FarmaVida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaVidaApplication.class, args);
+	}
+
+}
